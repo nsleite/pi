a8baa7f6de781f9800a2e361479ee7b7c731b885
Nivaldo Leite <83840192+nsleite@users.noreply.github.com>
2022-03-27 19:31:05 -0300

    Create led.py
    create the file that contains the already used code

---
+++ led.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+import RPi.GPIO as GPIO
+import time
+PIN = 5
+
+GPIO.setmode(GPIO.LOW)
+GPIO.setup(PIN, GPIO.OUT, initial=GPIO.LOW)
+
+while True:
+
+	try:
+		print("LED ON...")
+		GPIO.output(PIN, GPIO.HIGH)
+		time.sleep(0.5)
+		
+		print("LED OFF...")
+		GPIO.output(PIN, GPIO.LOW)
+		time.sleep(0.5)
+
+	except KeyboardInterrupt:
+		GPIO.output(PIN, GPIO.LOW)
+		GPIO.cleanup()
